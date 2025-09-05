e8ac89877c4c643f23aff25852fff7e18b826655
Namandeep Singh <64595184+Namandeepsd@users.noreply.github.com>
2025-09-05 23:16:01 +0530

    Insertion and deletion of a Singly LL by value,position, head and tail

--- dataTypes/listEx1.cpp
+++ dataTypes/listEx1.cpp
@@ -105,23 +105,94 @@ Node *kthDelete(Node*  head, int position){
 }
 
 
-Node 
+Node *delByValue(Node *head, int value){
+    Node *temp = head;
+    if (head == nullptr) return nullptr;
+
+    if(temp->data == value){
+        head = head->next;
+        delete (temp);
+    }
+
+    else{
+        Node *beforeTemp = temp;
+        temp = temp->next;
+        while(temp!=nullptr){
+            if(temp->data == value){
+                beforeTemp->next = temp->next;
+                delete (temp);
+            }
+            beforeTemp = temp;
+            temp = temp->next;
+        }
+
+        //Handling the last case
+      //  cout << "Temp Case" << beforeTemp->data;
+        if(beforeTemp->data == value){
+            Node *p = head;
+            while(p->next->next!=nullptr){
+                p = p->next;
+            }
+            Node *k = p->next;
+            p->next = p->next->next;
+            delete (k);
+        }
+    }
+    return head;
+}
+
+Node *insertAtHead(Node *head, int val){
+    Node *temp = new Node(val, head);
+    //temp->next = head;
+    //head = temp;
+    return temp;
+}
+Node *insertionAtTail(Node *head, int val){
+    Node *q = new Node(val,nullptr);
+    Node *temp = head;
+    while(temp->next!=nullptr){
+        temp = temp->next;
+    }
+    temp->next = q;
+    return head;
+}
+Node *insertionAtKth(Node *head, int val,int k){
+    Node *value = new Node(val);
+    Node *temp = head;
+    if(k==0){
+        value->next = head;
+        head = value;
+    }
+    else{
+    for (int i = 1; i < k && temp->next != nullptr;i++){
+        temp = temp->next;
+    }
+    value->next = temp->next;
+    temp->next = value;
+}
+    return head;
+}
+
 
 int main(){
-    vector<int> arr = {12, 5, 8, 7,6,8,9};
+    vector<int> arr = {12, 5, 8, 7,6};
     Node* head = convertArr2LL(arr);
   //  head = deleteHead(head);
     int length = lengthOfLL(head);
     //head = deleteTailLogic2(head);
-    head = kthDelete(head, 1);
-
+   // head = kthDelete(head, 1);
+   // head = delByValue(head, 8);
+    //head = insertAtHead(head,100);
+    //head = insertionAtTail(head,11);
+    head = insertionAtKth(head, 15, 0);
     // cout << head->data;
 
-   
+    // cout << delByValue(head,9)->data;
 
-//    cout << "Length of LL is:" << lengthOfLL(head) << "  ";
+    //    cout << "Length of LL is:" << lengthOfLL(head) << "  ";
 
-     // Traversing
+  //  Traversing
+  cout <<"\n ";
        Node *temp = head;
         while(temp!=nullptr){ 
         cout << temp->data <<" ";
